93e05f52cc172f7baa69a3c17c10429c3769a1e6
ClaitonBarchet <claitonbarchet@live.com>
2024-02-19 15:11:55 -0300

    curso 19-02-2023

--- CURSO_RX_JS/funcoes/desafio_1.js
+++ CURSO_RX_JS/funcoes/desafio_1.js
@@ -1,16 +1,40 @@
 // somar(3)(4)(5)
 
-const valores = (3, 4, 5)
-
-function somar (a, b, c){
-    return a + b
+function somar (a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c
+        }
+    }
 }
 
-const resultado = somar(3, 4, 5)
+const somarAB = somar(3)(4)
+console.log(somarAB(13))
+console.log(somar(13)(20)(30))
 
-console.log(resultado)
 
 // fn -> 3 * 7
 // fn -> 3 + 7
 // fn -> 3 -7
 // calcular(3)(7)(fn)
+
+function calcular(x) {
+    return function(y) {
+        return function (fn) {
+            return fn(x,y)
+        }
+    }
+}
+
+function subtrair (a,b) {
+    return a - b
+}
+
+function multiplicar (a,b) {
+    return a * b
+}
+
+const r1 = calcular(10)(5)(subtrair)
+const r2 = calcular(10)(5)(multiplicar)
+console.log(r1)
+console.log(r2)
